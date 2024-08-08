461a3989f8a40830b7547602825e652304f65f9e
Елена Юрьева <iurelen@yandex.ru>
2024-08-08 16:21:55 +0500

    Documentation API

--- project/core/urls.py
+++ project/core/urls.py
@@ -19,10 +19,33 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
+]
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Products API",
+      default_version='v1',
+      description="Документация для приложения products проекта Города",
+      contact=openapi.Contact(email="user@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(AllowAny,),
+)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
