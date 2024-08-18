1e1cbbfc9587e3fc932617e6d29e9cb5cbb82491
Елена Юрьева <iurelen@yandex.ru>
2024-08-18 17:12:32 +0500

    swagger_auto_schema

--- project/products/views.py
+++ project/products/views.py
@@ -1,3 +1,5 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,6 +8,13 @@ from .serializers import ProductSerializer
 
 
 class ProductsListView(APIView):
+
+    @swagger_auto_schema(
+        operation_description="Получить список всех продуктов",
+        responses={
+            status.HTTP_200_OK: ProductSerializer(many=True)
+        }
+    )
     def get(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(
@@ -13,4 +22,4 @@ class ProductsListView(APIView):
             many=True,
             context={'request': request}
         )
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
